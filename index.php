94635e1431a7f272a24684043263d1350db8efb6
Brandon Hempel <brandonhempel@gmail.com>
2025-06-07 19:56:02 -0500

    found OV pdf file

--- index.php
+++ index.php
@@ -7,6 +7,33 @@
     <title>crawler</title>
 </head>
 <body>
-    <?php echo "Hello World!"; ?>
+    <?php
+    
+    // find Open Vacancy pdf file
+    $fetchUrl = "https://ngmnpublic.azurewebsites.us/open-vacancies/";
+    $urlContent = file_get_contents($fetchUrl);
+
+    $dom = new DOMDocument();
+    @$dom->loadHTML($urlContent);
+    $xpath = new DOMXPATH($dom);
+    $href = $xpath->evaluate("/html/body//a"); // get all links on the page
+
+    foreach ($href as $link){
+        $url = $link->getAttribute('href');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+
+        // get anchor tag text
+        $linkTitle = $link->nodeValue;
+        
+        if($linkTitle == 'Download Open Vacancy Report'){
+            $pdf = $url;
+
+            echo $pdf;
+        }
+        
+    }
+
+    ?>
+
 </body>
 </html>
